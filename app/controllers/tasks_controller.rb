a42ee556e094524cc5a742f2fe0066557a01417c
Ubuntu <ubuntu@ip-172-31-41-114.ap-northeast-1.compute.internal>
2022-10-14 04:13:36 +0000

    Create destroy function

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -7,4 +7,42 @@ class TasksController < ApplicationController
     def show
         @task = Task.find(params[:id])
     end
+    
+    def new
+        @task = Task.new
+    end
+    
+    def create
+        task_params = params.require(:task).permit(:title, :content)
+        # データ入りインスタンスの作成
+        @task = Task.new(task_params)
+        # DBに保存
+        if @task.save
+        # 詳細画面へリダイレクト
+            redirect_to tasks_path
+        else
+            # バリデーションエラーの時に
+           render 'new', status: :unprocessable_entity 
+        end
+    end
+    
+    def edit
+        @task = Task.find(params[:id])
+    end
+    
+    def update
+        @task = Task.find(params[:id])
+        task_params = params.require(:task).permit(:title, :content)
+        if @task.update(task_params)
+           redirect_to tasks_path 
+        else
+            reder :edit
+        end
+    end
+    
+    def destroy
+        @task = Task.find(params[:id])
+        @task.destroy
+        redirect_to tasks_path
+    end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,8 +3,26 @@ Rails.application.routes.draw do
   root "tasks#index"
   get "/tasks", to: "tasks#index"
   
-  # # 詳細画面
+  # 新規作成
+  get "/tasks/new", to: "tasks#new", as: "new_task"
+  
+  # 編集
+  get "/tasks/:id/edit", to: "tasks#edit", as: "edit_task"
+  
+  # 編集更新
+  patch "tasks/:id", to: "tasks#update"
+  
+  # 詳細画面
   get "/tasks/:id", to: "tasks#show", as: "task"
-  # resources :tasks
+  
+  # 新規登録
+  post "/tasks", to: "tasks#create"
+  
+  # 削除
+  delete "/tasks/:id", to: "tasks#destroy"
+  
+  resources :tasks
+  
+  
   
 end
