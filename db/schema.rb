8c75db3052e3e89282a8d48dddb82dfca02d3148
Ubuntu <ubuntu@ip-172-31-41-114.ap-northeast-1.compute.internal>
2022-11-08 22:39:28 +0000

    Userモデルの作成

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_10_13_224935) do
+ActiveRecord::Schema[7.0].define(version: 2022_11_08_223830) do
   create_table "tasks", force: :cascade do |t|
     t.string "title"
     t.text "content"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "name"
+    t.string "email"
+    t.string "password_digest"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
